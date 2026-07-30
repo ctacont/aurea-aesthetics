ce17c9e9507596ddcca42d4d39cb91626c7742c4
hy <hy@brandtiger.de>
2026-07-30 18:24:16 +0000

    Visual edits

--- src/components/home/Pillars.jsx
+++ src/components/home/Pillars.jsx
@@ -9,7 +9,7 @@ export default function Pillars() {
     <section className="relative overflow-hidden px-6 py-24 lg:px-12 lg:py-40">
       <div className="pointer-events-none absolute inset-0" aria-hidden="true">
         <Image src={IMAGES.texture} alt="" className="h-full w-full scale-110 object-cover blur-[6px]" fittingType="fill" />
-        <div className="absolute inset-0 bg-[#F8F6F3]/8" />
+        <div className="absolute inset-0 bg-[#F8F6F3]/75" />
       </div>
 
       <div className="relative mx-auto max-w-6xl">
