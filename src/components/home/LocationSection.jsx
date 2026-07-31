28c4befc2380e1c7b38ff2e4c2f5afe2bcc61eb9
hy <hy@brandtiger.de>
2026-07-31 10:14:54 +0000

    Visual edits

--- src/components/home/LocationSection.jsx
+++ src/components/home/LocationSection.jsx
@@ -12,7 +12,7 @@ export default function LocationSection({ settings }) {
   return (
     <section className="grid lg:grid-cols-2">
       <Reveal className="order-2 lg:order-1">
-        <div className="relative h-[46svh] w-full lg:h-full lg:min-h-[42rem]">
+        <div className="h-[46svh] w-full lg:h-full lg:min-h-[42rem] absolute">
           <Image
             src={IMAGES.zurich}
             alt={t('locationSection.alt')}
