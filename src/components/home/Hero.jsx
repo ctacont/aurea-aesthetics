270b92cdb607165bb037554e250c75e935e8c853
hy <hy@brandtiger.de>
2026-07-31 16:04:07 +0000

    Manual code change

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -14,9 +14,10 @@ export default function Hero({ settings }) {
 
   const slides = [
   //{ src: settings.hero_image_url || IMAGES.hero, focalPointX: 0.88, focalPointY: 0.42 },
-  { src: IMAGES.hero, focalPointX: 0.88, focalPointY: 0.42 },
+  
   { src: IMAGES.interior, focalPointX: 0.5, focalPointY: 0.5 },
-  { src: IMAGES.zurich, focalPointX: 0.5, focalPointY: 0.5 }];
+  { src: IMAGES.zurich, focalPointX: 0.5, focalPointY: 0.5 },
+  { src: IMAGES.hero, focalPointX: 0.88, focalPointY: 0.42 }];
 
 
   const [currentIndex, setCurrentIndex] = useState(0);
