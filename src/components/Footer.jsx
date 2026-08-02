4de75af3552cd69b629997573a7ac685fdf3cce8
hy <hy@brandtiger.de>
2026-08-02 16:15:42 +0000

    Visual edits

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,7 +30,7 @@ export default function Footer() {
               loading="lazy"
               referrerpolicy="no-referrer-when-downgrade"
               allowFullScreen
-              className="h-[200px] w-full rounded-[2px] border border-white/10 grayscale contrast-[0.92] opacity-" />
+              className="h-[200px] w-full rounded-[2px] border border-white/10 grayscale opacity-90 contrast-[.92]" />
             
           </a>
         </div>
