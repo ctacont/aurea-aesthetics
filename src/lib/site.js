fdd63c2879684288a6986905088d9c7bdeba4faa
hy <hy@brandtiger.de>
2026-08-02 16:53:53 +0000

    Manual code change

--- src/lib/site.js
+++ src/lib/site.js
@@ -1,7 +1,7 @@
 export const IMAGES = {
   hero: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/9554c0ea5_startseite_hero_bild_v2_comp.jpg',
   texture: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/8c5458ab5_generated_image.png',
-  zurich: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/3b7d2bd8c_hero_bild_02_darker.jpg',
+  zurich: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/1644d560f_hero-aurea-standort_comp.jpg',
   interior: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/8ccbab271_hero_bild_03_darker.jpg',
   mimic: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/9147ba321_generated_image.png',
   filler: 'https://media.base44.com/images/public/6a6b131f6cc49b3ee60e929e/aa2efde7c_generated_image.png',
