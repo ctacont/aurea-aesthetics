2d8fd567e1dda5bf845c43f1282d7210fd3d4afd
hy <hy@brandtiger.de>
2026-08-07 10:20:47 +0000

    Manual code change

--- src/components/home/Philosophy.jsx
+++ src/components/home/Philosophy.jsx
@@ -25,7 +25,7 @@ export default function Philosophy() {
         </Reveal>
 
         <div className="mt-16 grid gap-14 lg:grid-cols-12 lg:gap-16">
-          <Reveal delay={140} className="lg:col-span-5  -translate-x-12">
+          <Reveal delay={340} className="lg:col-span-5  -translate-x-12">
             <div ref={parallaxRef} className="group relative aspect-[4/5] w-full overflow-hidden hasan10">
               <div style={{ transform: `translateY(${offset}px) scale(1.15)` }} className="h-full w-full hasan1">
                 <Image
